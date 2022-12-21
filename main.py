944246a33ec01111e503233d59c4a7fca5e7e30e
ct-kt <ct4g00gle@gmain.com>
2022-12-22 00:03:36 +0530

    write scripts in all __init__.py file

--- main.py
+++ main.py
@@ -24,25 +24,37 @@
 # for idx, record in enumerate(all_record):
 #      print(f"{idx}: {record}")
 
-#=========================================NEW CODE=========================================
+#=========================================Logger & Exception check=========================================
 
-from sensor.logger import logging
-from sensor.exception import SensorException
+# from sensor.logger import logging
+# from sensor.exception import SensorException
+# import sys, os
+
+# def test_logger_and_exception ():
+#      try:
+#           logging.info("Starting the test_logger_and_exception")
+#           result = 3/0
+#           print(result)
+#           logging.info("Stopping the test_logger_and_exception")
+#      except Exception as e:
+#           logging.debug(str(e))
+
+#           raise SensorException(e, sys)
+
+# if __name__ == "__main__":
+#      try:
+#           test_logger_and_exception()
+#      except Exception as e:
+#           print(e)
+
+
+#=========================================utils/get_collection_as_dataframe=========================================
+
+from sensor.utils import get_collection_as_dataframe
 import sys, os
-
-def test_logger_and_exception ():
-     try:
-          logging.info("Starting the test_logger_and_exception")
-          result = 3/0
-          print(result)
-          logging.info("Stopping the test_logger_and_exception")
-     except Exception as e:
-          logging.debug(str(e))
-
-          raise SensorException(e, sys)
 
 if __name__ == "__main__":
      try:
-          test_logger_and_exception()
+          get_collection_as_dataframe(database_name="aps", collection_name="sensor")
      except Exception as e:
           print(e)
