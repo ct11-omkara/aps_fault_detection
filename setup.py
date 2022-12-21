66d43a78babd0214fa3a406ce11518517a59e535
ct-kt <ct4g00gle@gmail.com>
2022-12-21 19:13:24 +0530

    Folder structure for project

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name="sensor",
     version="0.0.2",
     author="ineuron",
-    author_email="ctgorasiya@gmail.com",
+    author_email="avnish@ineuron.in",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
