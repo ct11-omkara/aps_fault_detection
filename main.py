8d99f1aaeae9cbfb5b846e9daa1a5f8b141df9e1
ct-kt <ct4g00gle@gmail.com>
2022-12-21 20:31:27 +0530

    test logger & exception in main.py

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
-import pymongo
+#=========================================OLD CODE=========================================
+# import pymongo
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
-# Database Name
-dataBase = client["neurolabDB"]
+# # Provide the mongodb localhost url to connect python to mongodb.
+# client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+# # Database Name
+# dataBase = client["neurolabDB"]
 
-# Collection  Name
-collection = dataBase['Products']
+# # Collection  Name
+# collection = dataBase['Products']
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
+# # Sample data
+# d = {'companyName': 'iNeuron',
+#      'product': 'Affordable AI',
+#      'courseOffered': 'Machine Learning with Deployment'}
 
-# Insert above records in the collection
-rec = collection.insert_one(d)
+# # Insert above records in the collection
+# rec = collection.insert_one(d)
 
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
+# # Lets Verify all the record at once present in the record with all the fields
+# all_record = collection.find()
 
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+# # Printing all records present in the collection
+# for idx, record in enumerate(all_record):
+#      print(f"{idx}: {record}")
+
+#=========================================NEW CODE=========================================
+
+from sensor.logger import logging
+from sensor.exception import SensorException
+import sys, os
+
+def test_logger_and_exception ():
+     try:
+          logging.info("Starting the test_logger_and_exception")
+          result = 3/0
+          print(result)
+          logging.info("Stopping the test_logger_and_exception")
+     except Exception as e:
+          logging.debug(str(e))
+
+          raise SensorException(e, sys)
+
+if __name__ == "__main__":
+     try:
+          test_logger_and_exception()
+     except Exception as e:
+          print(e)
