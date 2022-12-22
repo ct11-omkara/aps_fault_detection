c09aaf4b992f92ae8a0dd995ca4fb55cb1d01086
ct-kt <ct4g00gle@gmail.com>
2022-12-22 16:19:03 +0530

    Data Ingestion completed successfully...

--- main.py
+++ main.py
@@ -50,11 +50,42 @@
 
 #=========================================utils/get_collection_as_dataframe=========================================
 
-from sensor.utils import get_collection_as_dataframe
+# from sensor.utils import get_collection_as_dataframe
+# import sys, os
+
+# if __name__ == "__main__":
+#      try:
+#           get_collection_as_dataframe(database_name="aps", collection_name="sensor")
+#      except Exception as e:
+#           print(e)
+
+#=========================================config_entity.DataIngestionConfig testing=========================================
+
+# from sensor.entity import config_entity
+# import sys, os
+
+# if __name__ == "__main__":
+#      try:
+#           training_pipeline_config = config_entity.TrainingPipelineConfig()
+#           data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+#           print(data_ingestion_config.to_dict())
+#      except Exception as e:
+#           print(e)
+
+#=========================================components.data_ingestion.py (training & testing split) testing=========================================
+
+from sensor.entity import config_entity
+from sensor.components import data_ingestion
 import sys, os
 
 if __name__ == "__main__":
      try:
-          get_collection_as_dataframe(database_name="aps", collection_name="sensor")
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
+          
      except Exception as e:
           print(e)
+
