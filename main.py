8b87036185ab96106c4040a39fc02ed2d9de9373
ct-kt <ct4g00gle@gmail.com>
2022-12-25 00:23:16 +0530

    completed data validation

--- main.py
+++ main.py
@@ -74,8 +74,27 @@
 
 #=========================================components.data_ingestion.py (training & testing split) testing=========================================
 
+# from sensor.entity import config_entity
+# from sensor.components import data_ingestion
+# import sys, os
+
+# if __name__ == "__main__":
+#      try:
+#           training_pipeline_config = config_entity.TrainingPipelineConfig()
+#           data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+#           print(data_ingestion_config.to_dict())
+#           data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
+#           print(data_ingestion.initiate_data_ingestion())
+          
+#      except Exception as e:
+#           print(e)
+
+
+#=========================================components.data_validation.py (validation) testing=========================================
+
 from sensor.entity import config_entity
-from sensor.components import data_ingestion
+from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 import sys, os
 
 if __name__ == "__main__":
@@ -83,9 +102,15 @@ if __name__ == "__main__":
           training_pipeline_config = config_entity.TrainingPipelineConfig()
           data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
-          data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation = DataValidation(data_validation_config=data_validation_config, 
+                                           data_ingestion_artifact=data_ingestion_artifact)
+          data_validation_artifact = data_validation.initiate_data_validation()           
           
      except Exception as e:
           print(e)
+
 
